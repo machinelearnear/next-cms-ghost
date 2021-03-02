c4c6a6eb730cff148e18576b5ce051c44920952c
Nicolas Metallo <nicolas.metallo@nyu.edu>
2021-03-02 19:39:41 +0000

    update params

--- appConfig.ts
+++ appConfig.ts
@@ -8,7 +8,7 @@ export const fileCache: boolean = true
 
 // Dark mode
 export type DarkMode = 'dark' | 'light' | null
-export const defaultMode: DarkMode = 'light'
+export const defaultMode: DarkMode = 'dark'
 export const overrideOS: boolean = true
 
 // Images
